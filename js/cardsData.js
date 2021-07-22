9d3d65d4315b3575b00baf142aa59cee7c032b03
Irtradil <86210686+Irtradil@users.noreply.github.com>
2021-07-22 14:43:37 -0600

    Add files via upload

---
+++ js/cardsData.js
@@ -0,0 +1,52 @@
+let cardsData = [{
+  id: 1,
+  image: 'https://i.gifer.com/2a5l.gif',
+  avatar: 'https://avatars.githubusercontent.com/u/86210686?v=4',
+  user: {
+    name: 'irtradil',
+    title: 'Irtradil',
+    social: {
+        website: 'https://irtradil.me',
+    				github: "https://github.com/irtradil",
+    				ig: "https://instagram.com/irtradil.me",
+    				discord_id: "361175641032491010"
+    },
+    email: 'irtradil@hiroshilabs.com'
+  },
+  content: ''
+},
+{
+  id: 1,
+  image: 'https://data.1freewallpapers.com/detail/code-text-programming-coding.jpg',
+  avatar: 'https://avatars.githubusercontent.com/u/46204992?v=4',
+  user: {
+    name: 'jeanvides',
+    social: {
+        website: 'https://jeanvides.me',
+    				github: "https://github.com/JeanVydes",
+    				ig: "https://instagram.com/JeanVydes",
+    				discord_id: "525842461655040011"
+    },
+    title: 'Jean Vydes',
+    email: 'jean@hiroshilabs.com'
+  },
+  content: ''
+},
+{
+  id: 1,
+  image: 'https://data.whicdn.com/images/343421343/original.gif',
+  avatar: 'https://avatars.githubusercontent.com/u/71197922?v=4',
+  user: {
+    name: 'leonardtk',
+    social: {
+        website: 'https://mateochz.com',
+    				github: "https://github.com/LeonardTK",
+    				ig: "https://www.instagram.com/rip.leonardocg",
+    				discord_id: "624354017908359218"
+    },
+    title: 'Leonard TK',
+    email: 'leonard@hiroshilabs.com'
+  },
+  content: ''
+}]
+
